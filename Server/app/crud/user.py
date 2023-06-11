c879d91a89e14a1de8ba712e167d466c7df10e63
Harikeshav R <r.harikeshav@gmail.com>
2023-06-11 23:27:33 +0530

    Create user CRUD operations

---
+++ Server/app/crud/user.py
@@ -0,0 +1,62 @@
+import app.models.user as user_models
+import app.schemas.user as user_schemas
+from sqlalchemy.orm import Session
+
+
+def create_user(db: Session, user: user_schemas.UserCreate):
+    db_user = user_models.User(
+        user_name=user.user_name,
+        user_email=user.user_email,
+        user_phone=user.user_phone,
+        user_school=user.user_school,
+        user_attendance=False,
+        team_id=user.team_id,
+    )
+
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+
+    return db_user
+
+
+def read_user(db: Session, user_id: int):
+    db_user = db.query(user_models.User).filter(user_models.User.id == user_id).first()
+
+    return db_user
+
+
+def update_user(db: Session, user_id: int, user: user_schemas.UserUpdate):
+    db_user = db.query(user_models.User).filter(user_models.User.id == user_id).first()
+
+    if user.user_name:
+        db_user.user_name = user.user_name
+
+    if user.user_email:
+        db_user.user_email = user.user_email
+
+    if user.user_phone:
+        db_user.user_phone = user.user_phone
+
+    if user.user_school:
+        db_user.user_school = user.user_school
+
+    if user.user_attendance:
+        db_user.user_attendance = user.user_attendance
+
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+
+    return db_user
+
+
+def delete_user(db: Session, user_id: int):
+    db_user = db.query(user_models.User).filter(user_models.User.id == user_id).first()
+
+    name = db_user.user_name
+
+    db.delete(db_user)
+    db.commit()
+
+    return f"Deleted {name}!"
