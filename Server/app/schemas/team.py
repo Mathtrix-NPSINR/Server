0538021c673f830241bf64dcdc42a053e156eaf2
Harikeshav R <r.harikeshav@gmail.com>
2023-06-11 22:52:39 +0530

    Create team schema

---
+++ Server/app/schemas/team.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel
+from app.schemas.user import User
+
+
+class TeamBase(BaseModel):
+    team_name: str | None = None
+    team_school: str | None = None
+    team_event: str | None = None
+
+    team_members: list[User] | None = None
+
+
+class TeamCreate(TeamBase):
+    team_name: str
+    team_school: str
+    team_event: str
+
+
+class TeamUpdate(TeamBase):
+    team_name: str | None = None
+    team_school: str | None = None
+    team_event: str | None = None
+
+
+class TeamInDBBase(TeamBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+class Team(TeamInDBBase):
+    pass
