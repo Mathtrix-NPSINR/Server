9709b92fd689db6d92728525381c096930d3e05b
Harikeshav R <r.harikeshav@gmail.com>
2023-06-11 22:52:31 +0530

    Create user schema

---
+++ Server/app/schemas/user.py
@@ -0,0 +1,48 @@
+from app.schemas.team import Team
+from pydantic import BaseModel, EmailStr, constr
+
+
+class UserBase(BaseModel):
+    user_name: str | None = None
+    user_email: EmailStr | None = None
+    user_phone: constr(
+        regex="^\\+?\\d{1,4}?[-.\\s]?\\(?\\d{1,3}?\\)?[-.\\s]?\\d{1,4}[-.\\s]?\\d{1,4}[-.\\s]?\\d{1,9}$"
+    ) | None = None
+    user_school: str | None = None
+    user_attendance: bool | None = None
+
+    team_id: int | None = None
+    team: Team | None = None
+
+
+class UserCreate(UserBase):
+    user_name: str
+    user_email: EmailStr
+    user_phone: constr(
+        regex="^\\+?\\d{1,4}?[-.\\s]?\\(?\\d{1,3}?\\)?[-.\\s]?\\d{1,4}[-.\\s]?\\d{1,4}[-.\\s]?\\d{1,9}$"
+    )
+    user_school: str
+    user_attendance: bool = False
+
+    team_id: int
+
+
+class UserUpdate(UserBase):
+    user_name: str | None = None
+    user_email: EmailStr | None = None
+    user_phone: constr(
+        regex="^\\+?\\d{1,4}?[-.\\s]?\\(?\\d{1,3}?\\)?[-.\\s]?\\d{1,4}[-.\\s]?\\d{1,4}[-.\\s]?\\d{1,9}$"
+    ) | None = None
+    user_school: str | None = None
+    user_attendance: bool | None = None
+
+
+class UserInDBBase(UserBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+class User(UserInDBBase):
+    pass
