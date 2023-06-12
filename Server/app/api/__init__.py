8b60417d2b69ab16fbea8c0b8afc41b679d9bfcb
Harikeshav R <r.harikeshav@gmail.com>
2023-06-12 22:14:06 +0530

    Add user api router to main api router

--- Server/app/api/__init__.py
+++ Server/app/api/__init__.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 
-from app.api import team
+from app.api import team, user
 
 
 api_router = APIRouter(prefix="/api")
 api_router.include_router(team.router, prefix="/team", tags=["team"])
+api_router.include_router(user.router, prefix="/user", tags=["user"])
