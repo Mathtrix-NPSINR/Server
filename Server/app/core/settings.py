7bfc74761b3ec7f52bf0380a2c3c1de8cd47ee90
Harikeshav R <r.harikeshav@gmail.com>
2023-06-12 20:08:13 +0530

    Add qr codes storage directory field to settings

--- Server/app/core/settings.py
+++ Server/app/core/settings.py
@@ -10,7 +10,6 @@ class Settings:
     SQLALCHEMY_DATABASE_URL = "sqlite:///app/data/mathtrix.db"
     MATHTRIX_EMAIL_ADDRESS = os.getenv("MATHTRIX_EMAIL_ADDRESS")
     MATHTRIX_EMAIL_PASSWORD = os.getenv("MATHTRIX_EMAIL_PASSWORD")
-
-
+    QR_CODES_DIRECTORY = "app/data/qrcodes/"
 
 settings = Settings()
