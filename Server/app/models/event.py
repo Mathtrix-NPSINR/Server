933540326f7a75195aa6fc2b5b7e9d11729df235
Harikeshav R <r.harikeshav@gmail.com>
2023-06-15 23:32:52 +0530

    Create event model

---
+++ Server/app/models/event.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from app.core.db import Base
+
+
+class Event(Base):
+    __tablename__ = "events"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, nullable=False)
+
+    event_name: Mapped[str] = mapped_column(String, nullable=False, unique=True)
+    event_tagline: Mapped[str] = mapped_column(String, nullable=False)
+    event_description: Mapped[str] = mapped_column(String, nullable=False)
+    event_rules: Mapped[str] = mapped_column(String, nullable=False)
+    event_heads: Mapped[str] = mapped_column(String, nullable=False)
+    event_icon: Mapped[str] = mapped_column(String, nullable=False)
+
+    event_teams: Mapped[list["Team"]] = relationship()
+
