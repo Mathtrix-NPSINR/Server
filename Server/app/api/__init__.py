1bdbe378ddad2445f8d623275478ccbca1705467
Harikeshav R <r.harikeshav@gmail.com>
2023-06-15 23:55:07 +0530

    Add event API router to main API router

--- Server/app/api/__init__.py
+++ Server/app/api/__init__.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 
-from app.api import team, user
+from app.api import event, team, user
 
 api_router = APIRouter(prefix="/api")
+api_router.include_router(event.router, prefix="/event", tags=["event"])
 api_router.include_router(team.router, prefix="/team", tags=["team"])
 api_router.include_router(user.router, prefix="/user", tags=["user"])
