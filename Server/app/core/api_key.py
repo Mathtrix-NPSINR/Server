c5690ada6f79d96b2b1bcd5e874382f666d29d05
Harikeshav R <r.harikeshav@gmail.com>
2023-06-17 13:16:35 +0530

    Create get API key dependency

---
+++ Server/app/core/api_key.py
@@ -0,0 +1,20 @@
+from app.core.db import get_db
+from app.crud.api_key import read_api_key
+from fastapi import Depends, HTTPException, Security, status
+from fastapi.security import APIKeyQuery
+from sqlalchemy.orm import Session
+
+api_key = APIKeyQuery(name="api-key", auto_error=False)
+
+
+def get_api_key(api_key: str = Security(api_key), db: Session = Depends(get_db)) -> str:
+    db_api_key = read_api_key(db=db, key=api_key)
+
+    if not db_api_key:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid or missing API Key",
+        )
+
+    return db_api_key
+
