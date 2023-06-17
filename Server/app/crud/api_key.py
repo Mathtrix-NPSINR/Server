268a04e3c6cae0894a2fbddbad59db189f9e4e18
Harikeshav R <r.harikeshav@gmail.com>
2023-06-17 13:09:55 +0530

    Create API key CRUD operations

---
+++ Server/app/crud/api_key.py
@@ -0,0 +1,24 @@
+from sqlalchemy.orm import Session
+
+import secrets
+import app.models.api_key as api_key_models
+
+
+def create_api_key(db: Session, user: str):
+    db_api_key = api_key_models.APIKey(
+            user=user,
+            key=secrets.token_urlsafe(16)
+            )
+
+    db.add(db_api_key)
+    db.commit()
+    db.refresh(db_api_key)
+
+    return db_api_key
+
+
+def read_api_key(db: Session, key: str):
+    db_api_key = db.query(api_key_models.APIKey).filter(api_key_models.APIKey.key == key).first()
+
+    return db_api_key
+
