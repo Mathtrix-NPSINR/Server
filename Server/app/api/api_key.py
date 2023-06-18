83f77aee95f928172a80fa2e49cbdda42a21bca3
Harikeshav R <r.harikeshav@gmail.com>
2023-06-19 00:06:43 +0530

    Add logging to API key endpoint

--- Server/app/api/api_key.py
+++ Server/app/api/api_key.py
@@ -12,9 +12,11 @@ router = APIRouter()
 
 @router.post("/", response_model=dict)
 async def create_api_key_endpoint(*, db: Session = Depends(get_db), user: str):
+    logger.info(f"Creating a new API key for {user}!")
     return create_api_key(db=db, user=user).__dict__
 
 
 @router.get("/")
 async def check_api_key_endpoint(*, api_key=Security(get_api_key)):
+    logger.info(f"{api_key.user} has successfully logged in!")
     return api_key
